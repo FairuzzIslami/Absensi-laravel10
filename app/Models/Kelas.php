79bdb92cd42eeaad478464f0122ed9dfa2b91759
Fairuz Aqila Islami <firdauz2007123@gmail.com>
2025-07-24 22:44:18 +0700

    ubah model bagian users untuk siswa saja

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -8,12 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Kelas extends Model
 {
     use HasFactory;
-      protected $table = 'kelas';
+    protected $table = 'kelas';
     protected $primaryKey = 'id_kelas';
     protected $fillable = ['kelas', 'jurusan'];
 
+    public function allUsers()
+    {
+        return $this->hasMany(User::class, 'id_kelas'); // Semua user
+    }
+
     public function users()
     {
-        return $this->hasMany(User::class, 'id_kelas');
+        return $this->hasMany(User::class, 'id_kelas')->where('id_role', 3); // Hanya siswa
     }
 }
