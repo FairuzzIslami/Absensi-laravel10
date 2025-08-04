379619fa676f7a27734de810b05da937326af1d1
Fairuz Aqila Islami <firdauz2007123@gmail.com>
2025-08-04 08:36:51 +0700

    final project

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,8 +18,20 @@ class UserSeeder extends Seeder
         User::create([
             'username' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('123'), // <- ini di-hash secara aman
-            'id_role' => 1
+            'password' => Hash::make('123456'), // <- ini di-hash secara aman
+            'id_role' => 1          // admin
+        ]);
+         User::create([
+            'username' => 'Rini',
+            'email' => 'rini@gmail.com',
+            'password' => Hash::make('123456'),
+            'id_role' => 2      // guru
+        ]);
+         User::create([
+            'username' => 'Restu',
+            'email' => 'restu@gmail.com',
+            'password' => Hash::make('123456'),
+            'id_role' => 3    // siswa
         ]);
     }
 }
